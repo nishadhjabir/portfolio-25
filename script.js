2afd8015fa19d1afaa73f60cf3dc7df3ded8c8bc
Nishadh Jabir <nishadjabir@gmail.com>
2025-08-15 21:15:41 +0530

    resume change

--- script.js
+++ script.js
@@ -55,3 +55,10 @@ if (navbarLogo) {
   });
 }
 
+document.querySelectorAll('.cvbtn').forEach(btn => {
+  btn.addEventListener('click', e => {
+    btn.classList.add('active-hold');
+    setTimeout(() => btn.classList.remove('active-hold'), 1000); // 0.5s hold
+  });
+});
+
